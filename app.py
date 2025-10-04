872c9537a860bed25421814079d9757699987685
mr0kk <radoslawmrok@gmail.com>
2025-10-04 23:17:16 +0200

    start

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "strona glowna"
+
+# Trasa z parametrem
+@app.route('/about')         # Strona /about
+def about():
+    return "O nas"
+
+@app.route('/user/<name>')   # Parametr w URL
+def user_profile(name):
+    return f"Profil użytkownika: {name}"
+
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        return "Formularz został wysłany!"
+    else:
+        return "Wyświetl formularz kontaktowy"
+
+# Uruchamianie aplikacji
+if __name__ == '__main__':
+    app.run(debug=True)
